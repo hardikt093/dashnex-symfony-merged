af0baaae4dc386c73ae50e64b4e4b1c06b48e263
hardikt093 <hardikt093@gmail.com>
2023-02-10 19:14:13 +0530

    created functionality for cart crud -  added edit, delete and create product - added testcases for product get and single product get and cart crud

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/api', name: 'api_')]
+ class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'app_product',  methods: ['GET'])]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        $products = $doctrine
+            ->getRepository(Product::class)
+            ->findAll();
+  
+        $data = [];
+  
+        foreach ($products as $product) {
+           $data[] = [
+            'id' => $product->getId(),
+            'title' => $product->getTitle(),
+            'image' => $product->getImage(),
+            'price' => $product->getPrice(),
+            'description' => $product->getDescription(),
+            'created_at' => $product->getCreatedAt(),
+            'updated_at' => $product->getUpdatedAt(),
+           ];
+        }
+
+        return $this->json(
+            [ 
+                'status' => true, 
+                'msg' => '', 
+                'data' => $data
+            ]
+        );
+  
+        return $this->json($data);
+    }
+
+    #[Route('/product', name: 'new_product',  methods: ['POST'])]
+    public function new(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $parameters = json_decode($request->getContent(), true);
+
+        $entityManager = $doctrine->getManager();
+  
+        $date = new \DateTime('@'.strtotime('now'));
+
+        $product = new Product();
+        $product->setTitle($parameters['title']);
+        $product->setImage($parameters['image']);
+        $product->setPrice($parameters['price']);
+        $product->setDescription($parameters['description']);
+        $product->setCreatedAt($date);
+        $product->setUpdatedAt($date);
+  
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json(
+            [ 
+                'status' => true, 
+                'msg' => '', 
+                'data' => [
+                    'id' => $product->getId(),
+                    'title' => $product->getTitle(),
+                    'image' => $product->getImage(),
+                    'price' => $product->getPrice(),
+                    'description' => $product->getDescription(),
+                    'created_at' => $product->getCreatedAt(),
+                    'updated_at' => $product->getUpdatedAt(),
+                ]
+            ]
+        );
+    }
+
+    #[Route('/product/{id}', name: 'show_product',  methods: ['GET'])]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $product = $doctrine->getRepository(Product::class)->find($id);
+  
+        if (!$product) {
+            return $this->json(['status' => false, 'msg' => 'Product Not Found', 'data' => []]);
+        }
+  
+        $data =  [
+            'id' => $product->getId(),
+            'title' => $product->getTitle(),
+            'image' => $product->getImage(),
+            'price' => $product->getPrice(),
+            'description' => $product->getDescription(),
+            'created_at' => $product->getCreatedAt(),
+            'updated_at' => $product->getUpdatedAt(),
+        ];
+
+        return $this->json(
+            [ 
+                'status' => true, 
+                'msg' => '', 
+                'data' => $data
+            ]
+        );
+          
+    }
+
+    #[Route('/product/{id}', name: 'edit_product',  methods: ['PUT'])]
+    public function edit(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+  
+        if (!$product) {
+            return $this->json(['status' => false, 'msg' => 'Product Not Found', 'data' => []]);
+        }
+
+        $parameters = json_decode($request->getContent(), true);
+        $date = new \DateTime('@'.strtotime('now'));
+
+        $product->setTitle($parameters['title']);
+        $product->setImage($parameters['image']);
+        $product->setPrice($parameters['price']);
+        $product->setDescription($parameters['description']);
+        $product->setUpdatedAt($date);
+  
+        $product->setName($request->request->get('name'));
+        $product->setDescription($request->request->get('description'));
+        $entityManager->flush();
+  
+        $data =  [
+            'id' => $product->getId(),
+            'title' => $product->getTitle(),
+            'image' => $product->getImage(),
+            'price' => $product->getPrice(),
+            'description' => $product->getDescription(),
+            'created_at' => $product->getCreatedAt(),
+            'updated_at' => $product->getUpdatedAt(),
+        ];
+          
+        return $this->json(
+            [ 
+                'status' => true, 
+                'msg' => '', 
+                'data' => $data
+            ]
+        );
+    }
+
+    #[Route('/product/{id}', name: 'delete_product',  methods: ['DELETE'])]
+    public function delete(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+  
+        if (!$product) {
+            return $this->json(['status' => false, 'msg' => 'Product Not Found', 'data' => []]);
+        }
+  
+        $entityManager->remove($product);
+        $entityManager->flush();
+  
+        return $this->json(['status' => true, 'msg' => 'Deleted a product successfully with id ' . $id, 'data' => []]);
+    }
+}
--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -2,37 +2,117 @@
 
 namespace App\Entity;
 
+use App\Repository\CartRepository;
+use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use App\Controller\CartController;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Delete;
-use App\State\CartStateProvider;
-use App\State\CartStateProcessor;
-use Doctrine\Common\Collections\Collection;
 
-#[ApiResource(
-    operations: [
-        new Get( provider: CartStateProvider::class, uriTemplate: '/cart'),
-        new Put(processor: CartStateProcessor::class, uriTemplate: '/cart'),
-        new Delete(processor: CartStateProcessor::class, uriTemplate: '/cart'),
-    ]
-)]
+#[ORM\Entity(repositoryClass: CartRepository::class)]
+#[ApiResource(operations: [
+    new GetCollection(
+        routeName:'api_app_cart'
+    ),
+    new Get(
+        name: 'cart/{id}',
+        routeName:'api_show_cart'
+    ),
+    new Post(
+        name: 'cart',
+        routeName:'api_new_cart',
+        controller: CartController::class
+    ),
+    new Put(
+        name: 'cart/{id}',
+        routeName:'api_edit_cart',
+    ),
+    new Delete(
+        name: 'cart',
+        routeName:'api_delete_cart',
+    )
+])]
+
 class Cart
 {
-    public Collection $items;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    public function getItems()
+    #[ORM\OneToOne(cascade: ['persist'], fetch:'EAGER')]
+    #[ORM\JoinColumn(name:'product_id', referencedColumnName:'id', nullable:false)]
+    private ?Product $product = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function __construct()
     {
-        return $this->items;
+        $date = new \DateTimeImmutable('@'.strtotime('now'));
+        $this->created_at =  $date;
+        $this->updated_at =  $date;
     }
 
-    public function addItem(CartItem $cartItem) {
-        $this->items->removeElement($cartItem);
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product): self
+    {
+        $this->product = $product;
 
         return $this;
     }
 
-    public function removeItem(CartItem $cartItem) {
-        $this->items->add($cartItem);
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
     }
 }
